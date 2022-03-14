426e219fbabc70286d147f1b083807c1beeea6d1
Danil Prokopenko <haosprinz@gmail.com>
2022-03-14 21:01:59 +0500

    Переделал метод echo

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,19 +1,30 @@
+from datetime import datetime
+
 from aiogram import types
-from loader import dp, bot
+from sqlalchemy.orm import sessionmaker
+
+from data.config import ENGINE
+from loader import dp
 from utils.yandex_api.connect import connect
+from data.models import Dialogs
 
 
 # Эхо хендлер, куда летят текстовые сообщения без указанного состояния
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
+    session = sessionmaker(bind=ENGINE)()
+
+    db_request_dialogs = Dialogs(
+        user_text=message.text,
+        user_id=int(message.from_user.id),
+        user_name=message.from_user.full_name,
+        date=datetime.now()
+    )
+
+    session.add(db_request_dialogs)
+    session.commit()
+    session.close()
     await message.answer(connect(message.text))
-
-
-# Делаем ответ полученным стикером
-@dp.message_handler(content_types=['sticker'])
-async def bot_sticker(message):
-    sticker_id = message.sticker.file_id
-    await bot.send_sticker(message.chat.id, sticker_id)
 
 
 # Ответ на не прописанные действия
